0dc5f8e55a6a610098ae9e796aceeba632fe8e48
Nick-UI <var30000@mail.ru>
2022-03-17 22:02:53 +0300

    add a new bug

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 import re
 from urllib.parse import urlparse
 import urllib.request
@@ -5,11 +6,12 @@ import datetime
 
 class Crawler:
 
-    def __init__(self, site: str) -> None:
+    def __init__(self, site: str, threads: int = 6) -> None:
         self.site = site
         self.host = urlparse(site).netloc
         self.queue = [site]
         self.visited = []
+        self.threads = threads
 
     def is_iternal(self, link: str) -> bool:
         return urlparse(link).netloc == self.host
@@ -18,6 +20,15 @@ class Crawler:
         conversion_links = []
         for link in links:
             if self.is_iternal(link):
+                continue
+            
+            if link in self.queue:
+                continue
+            
+            if link.startswith('?'):
+                continue
+
+            if link == '#':
                 continue
 
             link = '%20'.join(link.split())
@@ -37,41 +48,39 @@ class Crawler:
 
             if link.endswith('/'):
                 link = link[:-1]
-                if link.endswith('/'):
-                    link = link[:-1]
-                    if check:
-                        conversion_links.append(link)
-                        continue
-
                 if check:
                         conversion_links.append(link)
                         continue
 
-            if link == '#':
-                continue
-
         return conversion_links
 
+    def _read_link(self, link: str) -> None:
+        print('Processing ' + link)
+        try:
+            response = urllib.request.urlopen(link)
+        except:
+            return
+
+        if link in self.visited:
+            return
+
+        self.visited.append(link)
+        self.queue.remove(link)
+
+        page = str(response.read())
+        pattern = '<a [^>]*href=[\'|"](.*?)[\'"].*?>'
+    
+        links = re.findall(pattern, page)
+    
+        self.queue.extend([link for link in self._link_conversion(links) if link not in self.visited and link not in self.queue and self.is_iternal(link)])
+
     def get_all_links(self) -> None:
-        for link in self.queue:
-            print('Proccesing ' + link)
+        self._read_link(self.site)
+        with ThreadPoolExecutor(200) as excecutor:
             try:
-                response = urllib.request.urlopen(link)
-            except:
-                continue
-
-            if link in self.visited:
-                continue
-
-            self.visited.append(link)
-            self.queue.remove(link)
-        
-            page = str(response.read())
-            pattern = '<a [^>]*href=[\'|"](.*?)[\'"].*?>'
-        
-            links = re.findall(pattern, page)
-        
-            self.queue.extend([link for link in self._link_conversion(links) if link not in self.visited and link not in self.queue and self.is_iternal(link)])
+                excecutor.map(self._read_link, self.queue, timeout=3)
+            except KeyboardInterrupt:
+                exit()
 
 
         
