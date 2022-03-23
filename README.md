c590bb6fcc9bba389bf36cf8b6b351ece6ac85fa
Nick-UI <var30000@mail.ru>
2022-03-23 14:41:52 +0300

    add files and fix#5

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # sitemap-pars
+<table border="1">
+   <tr>
+    <th>URL сайта</th>
+    <th>Время обработки</th>
+    <th>Кол-во найденных ссылок</th>
+    <th>Имя файла с результатом</th>
+   </tr>
+   <tr>
+    <td>http://crawler-test.com</td>
+    <td>83.043</td>
+    <td>394</td>
+    <td><a href="sitemap-pars\files\crawler-test.com.txt">crawler-test.com.txt</a></td>
+    </tr>
+    <td>http://google.com</td>
+    <td>14.447</td>
+    <td>83</td>
+    <td><a href="sitemap-pars\files\google.com.txt">google.com.txt</a></td>
+    </tr>
+    <td>https://vk.com</td>
+    <td>70.546</td>
+    <td>500</td>
+    <td><a href="sitemap-pars\files\vk.com.txt">vk.com.txt</a></td>
+    </tr>
+    <td>https://yandex.ru</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    </tr>
+    <td>https://stackoverflow.com</td>
+    <td>72.410</td>
+    <td>164</td>
+    <td><a href="sitemap-pars\files\stackoverflow.com.txt">stackoverflow.com.txt</a></td>
+    </tr>
+ </table>
--- crawler.py
+++ crawler.py
@@ -1,11 +1,13 @@
 import re
 import threading
+from urllib.error import HTTPError
 from urllib.parse import urlparse, urlsplit, urlunsplit, quote
 import urllib.request
 import time
+from socket import timeout
+
 
 class Crawler:
-
     def __init__(self, site: str, threads=None) -> None:
         self.site = site
         self.scheme = urlparse(site).scheme
@@ -13,6 +15,7 @@ class Crawler:
         self.visited = []
         self.queue = [site]
         self.threads = threads if threads else 1
+        self.not_response = []
 
     def is_iternal(self, link: str) -> bool:
         return urlparse(link).netloc == self.netloc
@@ -27,11 +30,11 @@ class Crawler:
 
             if url.path:
                 new_path = re.sub(r'//+', '/', quote(url.path.encode('utf8')))
-            
+
             if not url.path:
                 new_path = ''
 
-            if url.query:
+            if url.query or url.fragment:
                 if all([not url.netloc, not url.path]):
                     new_path = '/'
 
@@ -41,7 +44,15 @@ class Crawler:
             if any([not url.netloc, url.netloc == self.netloc]):
                 new_netloc = self.netloc
 
-            url = url._replace(scheme=self.scheme,
+            if url.scheme:
+                new_scheme = url.scheme
+                if new_scheme not in ['http', 'https']:
+                    continue
+            
+            if not url.scheme:
+                new_scheme = self.scheme
+                
+            url = url._replace(scheme=new_scheme,
                                netloc=new_netloc,
                                path=new_path)
             
@@ -58,13 +69,23 @@ class Crawler:
 
         return conversion_links
 
+    def check(self, link: str) -> bool:
+        return all([link not in self.visited,
+                    link not in self.not_response,
+                    link not in self.queue])
+
     def _read_link(self, link: str) -> None:
         self.queue.pop(0)
         print('Processing ' + link)
 
         try:
-            response = urllib.request.urlopen(link)
-        except:
+            response = urllib.request.urlopen(link, timeout=5)
+        
+        except timeout:
+            return
+
+        except HTTPError:
+            self.not_response.append(link)
             return
 
         if link in self.visited:
@@ -82,7 +103,7 @@ class Crawler:
         if not links:
             return
         
-        self.queue.extend([link for link in links if link not in self.visited and link not in self.queue])
+        self.queue.extend([link for link in links if self.check(link)])
 
     def get_all_links(self) -> None:
         while self.queue:
@@ -92,15 +113,27 @@ class Crawler:
                 if not self.queue:
                     break
 
-                thread = threading.Thread(target=self._read_link, args=(self.queue[0],))
+                thread = threading.Thread(target=self._read_link, 
+                                          args=(self.queue[0],))
                 thread.start()
                 threads.append(thread)
                             
             [thread.join() for thread in threads]
         
+    def save_to_txt(self) -> None:
+        with open(f'{self.netloc}.txt', 'x') as file:
+            file.write('\n'.join([link for link in sorted(self.visited)]))
+        
     
 if __name__ == '__main__':
+    url = ''
+    threads = 1
+
     start_time = time.time()
-    crawler = Crawler('https://crawler-test.com')
+    
+    crawler = Crawler(url, threads)
     crawler.get_all_links()
+
     print(time.time() - start_time)
+    
+    crawler.save_to_txt()
